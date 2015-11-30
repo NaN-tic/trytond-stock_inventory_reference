a7704c04d2dbc634f9a269e283508a67ceec19fe
resteve <resteve@zikzakmedia.com>
2015-12-01 00:46:12 +0100

    pyflakes

--- inventory.py
+++ inventory.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 from trytond.pyson import Eval, Not, Equal
 
 __all__ = ['Inventory']
